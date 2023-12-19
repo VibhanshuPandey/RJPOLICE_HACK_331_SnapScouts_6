3c4f8df32ef8e9c7107fa0fa1e8e9e24c1ca6c4d
Vinit <vinitchawda20@gmail.com>
2023-12-19 16:13:53 +0530

    added frontend files

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mydatabase"
+db = PyMongo(app).db
 
 @app.route('/')
 def hello_world():
+    db.new_doc.insert_one({"a": 1})
     return 'Hello, World!'
 
 app.run(debug=True)
